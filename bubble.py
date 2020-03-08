9cb8d5e6941b5b7e258684211a90b77884663d8b
Valeriia Zabkowska <kristofer90@gmail.com>
2020-03-08 23:34:19 +0100

    Bubble object updated

--- bubble.py
+++ bubble.py
@@ -4,30 +4,27 @@ from pygame.sprite import Sprite
 
 class Bubble(Sprite):
     '''Klasa przeznaczona do zarządzania bąbelkami wystrzeliwanymi przez delfina'''
-    def __init__(self,dolphingame_settings,screen,dolphin):
+    def __init__(self, dolphingame_settings, screen, dolphin):
 
         '''Utworzenie obiektu pocisku w aktualnym położeniu delfina'''
-        super(Bubble,self).__init__()
-        self.screen=screen
+        super(Bubble, self).__init__()
+        self.screen = screen
+        image = pygame.image.load('images/bubble.bmp')
+        self.image = pygame.transform.scale(image, (15, 15))
+        self.rect = pygame.Rect(0, 0, dolphingame_settings.bubble_width, dolphingame_settings.bubble_height)
 
-        self.rect=pygame.Rect(0,0, dolphingame_settings.bubble_width, dolphingame_settings.bubble_height)
-
-        self.rect.center=dolphin.rect.center
-        self.rect.top=dolphin.rect.centery
+        self.rect.center = dolphin.rect.center
+        self.rect.top = dolphin.rect.centery
 
         #polożenie pocisku jest zdefinowane za pomocą wartości zmiennoprzecinkowej
-        self.x=float(self.rect.x)
-
-
-        self.color=dolphingame_settings.bubble_color
-        self.speed_factor=dolphingame_settings.bubble_speed_factor
-
+        self.x = float(self.rect.x)
+        self.speed_factor = dolphingame_settings.bubble_speed_factor
 
     def update(self):
         '''Poruszanie pociskiem po ekranie'''
-        self.x-=self.speed_factor
-        self.rect.x=self.x
+        self.x-= self.speed_factor
+        self.rect.x = self.x
 
     def draw_bubble(self):
         '''Wyświetlenie pocisku na ekranie'''
-        pygame.draw.rect(self.screen,self.color,self.rect)
+        self.screen.blit(self.image, self.rect)
